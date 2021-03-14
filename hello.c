e68de85ae7ff31698346c69a253d16bbb0ab21c8
[SA-Bijoy] <[bijoykhanpp@gmail.com]>
2021-03-14 11:45:27 +0600

    added two file hello.c hello.exe

---
+++ hello.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+	int generate_number,given_number;
+	bool check_win = false;
+	time_t t;
+
+	srand((unsigned)time(&t));
+
+	generate_number = rand() % 10 + 1;
+
+	scanf("%d", &given_number);
+
+	if(generate_number == given_number){
+		
+		check_win  = true;
+	}else {
+		check_win = false;
+	}
+
+	if( check_win){
+		printf("You win\n");
+	}else {
+		printf("You loss and the generate number was %d\n", generate_number);
+	}
+
+	return 0;
+}
